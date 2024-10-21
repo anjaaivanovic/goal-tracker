709a6e1d7cdc74ae49310ffacddee561be653746
anja.ivanovic <anja.ivanovic02kg@gmail.com>
2024-10-21 17:31:57 +0200

    Initialized project, added JWT authorization and authentication.

--- src/main/java/com/example/goal_tracker/config/SecurityConfig.java
+++ src/main/java/com/example/goal_tracker/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.example.goal_tracker.config;
 
-import com.example.goal_tracker.auth.model.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
